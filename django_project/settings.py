06b270b6e50a7c3442631c425c29fa918cf333bb
Mohamed Sayed <mohamedsayedaz18@gmail.com>
2024-11-17 01:36:23 +0200

    Install django-crispy-forms , crispy-bootstrap5

--- django_project/settings.py
+++ django_project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # 3rd Party
+    "crispy_forms",
+    "crispy_bootstrap5",
+    # Local
     "accounts.apps.AccountsConfig",
     "pages.apps.PagesConfig",
 ]
@@ -126,3 +130,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "accounts.CustomUser"
 LOGIN_REDIRECT_URL = "home"
 LOGOUT_REDIRECT_URL = "home"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
